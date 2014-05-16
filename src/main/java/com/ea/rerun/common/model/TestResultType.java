40ff004f218dbb2224faaef73960980b368c3ee8
jackey90 <jackey90.hj@gmail.com>
2014-05-16 18:01:45 +0800

    baba

---
+++ src/main/java/com/ea/rerun/common/model/TestResultType.java
@@ -0,0 +1,5 @@
+package com.ea.rerun.common.model;
+
+public enum TestResultType {
+	Successed, Stable_Failed, UnStable_Failed, Skiped;
+}
--- src/main/java/com/ea/rerun/feedback/Result.java
+++
@@ -1,5 +0,0 @@
-package com.ea.rerun.feedback;
-
-public class Result {
-
-}
